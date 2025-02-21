945df39a9f0ee2caad01e4a5055f11dbe7ee6fea
Amal M Raju <166976243+amal0307@users.noreply.github.com>
2025-02-21 12:57:47 +0530

    Add files via upload

---
+++ Assignment-10/Question2.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const redButton = document.getElementById('redButton');
+    const blueButton = document.getElementById('blueButton');
+
+    redButton.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'red';
+    });
+
+    blueButton.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'blue';
+    });
+});
